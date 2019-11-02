6f985e3a1a00398a2c724089ba23e67019e44b97
daniii3012 <daniel_301999@hotmail.com>
2019-11-02 08:35:39 -0500

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './pages/home/home.component';
+import { ProductsComponent } from './pages/user/products/products.component';
+import { CartComponent } from './pages/user/cart/cart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'cart', component: CartComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductsComponent } from './pages/user/products/products.component';
+import { ShoppingHistoryComponent } from './pages/user/shopping-history/shopping-history.component';
+import { PurchaseComponent } from './pages/user/purchase/purchase.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { CartComponent } from './pages/user/cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductsComponent,
+    ShoppingHistoryComponent,
+    PurchaseComponent,
+    NavbarComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
