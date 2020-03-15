f88cc98efa9772cd14d2526c8f5767f3f176e284
daniii3012 <daniel_301999@hotmail.com>
2020-03-15 00:02:48 -0500

    pipe

--- src/app/pipes/filter/filter.pipe.ts
+++ src/app/pipes/filter/filter.pipe.ts
@@ -5,8 +5,17 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FilterPipe implements PipeTransform {
 
-  transform(value: any, ...args: any[]): any {
-    return null;
+  transform(value: any, arg: string): any {
+    const resultPosts = [];
+    if(value && arg.length > 2){
+      for(let post of value){
+        if(post.tags.toLowerCase().indexOf(arg.toLowerCase()) > -1) {
+          resultPosts.push(post);
+        }
+      }
+      return resultPosts;
+    }
+    
   }
 
 }
