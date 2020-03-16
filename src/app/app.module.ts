5dcd4df6ea4efce988b1eecd31a5f21ed7b0121f
daniii3012 <daniel_301999@hotmail.com>
2020-03-16 17:42:19 -0500

    creacion de paginas y nabvar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { StoreComponent } from './pages/store/store.component';
+import { AdminComponent } from './pages/admin/admin.component';
+import { UserComponent } from './pages/user/user.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductsComponent,
+    StoreComponent,
+    AdminComponent,
+    UserComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
